fdd551b1c13fdd114afd14774573324b55eb23f2
abdoul <abdiallos39@gmail.com>
2026-07-27 00:56:49 +0200

    Correction indentation route version

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ def hello():
 @app.route('/api/status')
 def status():
     return {"status": "en ligne", "version": "1.0"}
-    @app.route('/api/version')
+
+@app.route('/api/version')
 def version():
     return {"version": "1.1", "environnement": "dev"}
 
