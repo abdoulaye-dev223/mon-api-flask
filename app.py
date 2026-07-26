6981180a27cd7be5fb644c2ccf868c074c4783fb
abdoul <abdiallos39@gmail.com>
2026-07-27 00:18:46 +0200

    Ajout route version pour test multi-env

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ def hello():
 @app.route('/api/status')
 def status():
     return {"status": "en ligne", "version": "1.0"}
+    @app.route('/api/version')
+def version():
+    return {"version": "1.1", "environnement": "dev"}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
